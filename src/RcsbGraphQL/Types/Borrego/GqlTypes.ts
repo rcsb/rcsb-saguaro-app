d0df827a4c86dde59b964de566513115ead51f8f
bioinsilico <jsegurmor@gmail.com>
2020-05-25 13:50:20 -0700

    npm snapshots

--- src/RcsbGraphQL/Types/Borrego/GqlTypes.ts
+++ src/RcsbGraphQL/Types/Borrego/GqlTypes.ts
@@ -67,10 +67,10 @@ export enum FieldName {
 }
 
 export interface FilterInput {
-  operation?: Maybe<OperationType>,
-  source?: Maybe<Source>,
   values?: Maybe<Array<Maybe<Scalars['String']>>>,
   field?: Maybe<FieldName>,
+  operation?: Maybe<OperationType>,
+  source?: Maybe<Source>,
 }
 
 export interface Gap {
