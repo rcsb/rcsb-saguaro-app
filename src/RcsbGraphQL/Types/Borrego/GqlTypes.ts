d4469290a439480b65e34f5d03c6138b0b258380
bioinsilico <jsegurmor@gmail.com>
2021-01-27 13:28:46 -0800

    saguaro update

--- src/RcsbGraphQL/Types/Borrego/GqlTypes.ts
+++ src/RcsbGraphQL/Types/Borrego/GqlTypes.ts
@@ -109,10 +109,10 @@ export enum FieldName {
 }
 
 export interface FilterInput {
-  field?: Maybe<FieldName>;
-  source?: Maybe<Source>;
   values?: Maybe<Array<Maybe<Scalars['String']>>>;
   operation?: Maybe<OperationType>;
+  source?: Maybe<Source>;
+  field?: Maybe<FieldName>;
 }
 
 export enum OperationType {
