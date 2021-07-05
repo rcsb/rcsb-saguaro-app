af38a66d551b31965500f47ff42a0e82d65a5112
bioinsilico <jsegurmor@gmail.com>
2021-07-05 11:00:29 -0700

    saguaro-api graphql query validation query

--- src/RcsbGraphQL/Queries/QueryCheck.ts
+++ src/RcsbGraphQL/Queries/QueryCheck.ts
@@ -5,3 +5,4 @@ import * as configYosemiteGraphQL from "../../RcsbServerConfig/codegen.yosemite.
 import {validateQueries} from "@rcsb/rcsb-saguaro-api/build/RcsbGraphQL/Generator/GeneratorTools";
 
 validateQueries(configYosemiteGraphQL.schema,"src/RcsbGraphQL/Queries/Yosemite/*graphql");
+validateQueries(configBorregoGraphQL.schema,"src/RcsbGraphQL/Queries/Borrego/*graphql");
