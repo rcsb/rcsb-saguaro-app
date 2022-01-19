0afcff08a6fbf5cfbaa86b0efa529f80164a36b2
bioinsilico <jsegurmor@gmail.com>
2022-01-19 14:25:04 -0800

    msa conservation bg color

--- src/RcsbFvWeb/RcsbFvGroup/GroupSequenceTabs.tsx
+++ src/RcsbFvWeb/RcsbFvGroup/GroupSequenceTabs.tsx
@@ -298,7 +298,7 @@ function buildAlignmentVariation(): ExternalTrackBuilderInterface {
                     trackColor: "#F9F9F9",
                     displayColor: {
                         thresholds:[],
-                        colors:["#5289e9", "#76bbf6", "#91cef6", "#b9d9f8", "#d6eafd", "#e6f5fd", "#ececec"]
+                        colors:["#5289e9", "#76bbf6", "#91cef6", "#b9d9f8", "#d6eafd", "#e6f5fd", "#f9f9f9"]
                     },
                     trackHeight: 20,
                     titleFlagColor: RcsbAnnotationConstants.provenanceColorCode.rcsbPdb,
