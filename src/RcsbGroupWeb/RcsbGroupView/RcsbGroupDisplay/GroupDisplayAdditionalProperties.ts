816fd08afd840719b4e79cfa36452a5fe2adf375
bioinsilico <jsegurmor@gmail.com>
2022-03-24 16:33:02 -0700

    code refactoring

--- src/RcsbGroupWeb/RcsbGroupView/RcsbGroupDisplay/GroupDisplayAdditionalProperties.ts
+++
@@ -1,5 +0,0 @@
-import {ChartMapType} from "../../../RcsbChartWeb/RcsbChartView/RcsbChartLayout";
-
-export interface GroupDisplayAdditionalProperties {
-    componentMountCallback(chartMap: ChartMapType, layout:string[]):void;
-}
--- src/request.ts
+++
@@ -1,1 +0,0 @@
-export {rcsbRequestClient} from "./RcsbRequest/RcsbRequestClient";
