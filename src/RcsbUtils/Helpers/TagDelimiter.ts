c5e822d301bf165335e852167f734863e481dd30
bioinsilico <jsegurmor@gmail.com>
2023-01-17 10:40:38 -0800

    rcsbId parser bug fixed

--- src/RcsbUtils/Helpers/TagDelimiter.ts
+++ src/RcsbUtils/Helpers/TagDelimiter.ts
@@ -14,27 +14,43 @@ export class TagDelimiter {
 
     public static parseEntity(rcsbId:string): {entryId: string;entityId:string;} {
         const ids: string[] = rcsbId.split(TagDelimiter.entity);
-        const entityId: string | undefined = ids.pop();
-        if(!entityId)
+        if(ids.length < 2)
             throw `Entity Id ${rcsbId} format error`;
+        const entityId: string = ids.pop() as string;
         const entryId: string  =  ids.join(TagDelimiter.entity)
         return {entryId, entityId};
     }
 
     public static parseInstance(rcsbId:string): {instanceId: string;entryId:string;} {
         const ids: string[] = rcsbId.split(TagDelimiter.instance);
-        const instanceId: string | undefined = ids.pop();
-        if(!instanceId)
+        if(ids.length < 2)
             throw `Instance Id ${rcsbId} format error`;
+        const instanceId: string | undefined = ids.pop() as string;
         const entryId: string =  ids.join(TagDelimiter.instance)
         return {instanceId, entryId};
     }
 
     public static parseRcsbId(rcsbId:string): {instanceId?:string;entityId?:string;entryId:string;} {
-        if(this.rcsbEntityRegExp.test(rcsbId))
-            return this.parseEntity(rcsbId);
-        if(this.rcsbInstanceRegExp.test(rcsbId))
-            return this.parseInstance(rcsbId);
+        let entity: {entryId: string;entityId:string;} | undefined = undefined;
+        let ids: string[] = rcsbId.split(TagDelimiter.entity);
+        if(ids.length > 1){
+            entity = {
+                entityId: ids.pop() as string,
+                entryId: ids.join(TagDelimiter.entity)
+            };
+        }
+        let instance: {instanceId: string;entryId:string;} | undefined = undefined;
+        ids =  rcsbId.split(TagDelimiter.instance);
+        if(ids.length > 1){
+            instance = {
+                instanceId: ids.pop() as string,
+                entryId: ids.join(TagDelimiter.instance)
+            }
+        }
+        if(entity)
+            return {...entity};
+        if(instance)
+            return {...instance};
         return {entryId: rcsbId};
     }
 
