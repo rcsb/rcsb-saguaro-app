09c09ebe17249ea5bab5f96aff7dfb01284acd2d
bioinsilico <jsegurmor@gmail.com>
2023-03-13 09:17:49 -0700

    trackConfigModifier.alignment includes AlignmentResponse index

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,7 +18,7 @@ to add the new histogram/bar condition  to the current query
   - `add` to provide additional `UiComponentType` components
   - `replace` to replace default `UiComponentType` for external ones
 - `buildInstanceSequenceFv` parameter `InstanceSequenceConfig.defaultValue` points to label_asym_id instead of label_auth_id
-- `trackConfigModifier.alignment` includes the full alignment response `AlignmentResponse` as third parameter
+- `trackConfigModifier.alignment` includes the full alignment response `AlignmentResponse` and alignment member index as parameters
 
 ### Improvements
 - tsc strictNullChecks true
--- src/RcsbFvWeb/RcsbFvFactories/RcsbFvBlockFactory/AlignmentBlockFactory.ts
+++ src/RcsbFvWeb/RcsbFvFactories/RcsbFvBlockFactory/AlignmentBlockFactory.ts
@@ -7,14 +7,14 @@ import {BlockFactoryInterface} from "./BlockFactoryInterface";
 import {RcsbFvRowConfigInterface} from "@rcsb/rcsb-saguaro";
 import {TrackFactoryInterface} from "../RcsbFvTrackFactory/TrackFactoryInterface";
 
-export class AlignmentBlockFactory implements BlockFactoryInterface<[AlignmentRequestContextType, AlignmentResponse],[AlignmentRequestContextType, TargetAlignment, AlignmentResponse]> {
+export class AlignmentBlockFactory implements BlockFactoryInterface<[AlignmentRequestContextType, AlignmentResponse],[AlignmentRequestContextType, TargetAlignment, AlignmentResponse, number]> {
 
     readonly trackFactory: TrackFactoryInterface<[AlignmentRequestContextType, TargetAlignment]>;
-    readonly trackConfigModifier?: (alignmentContext: AlignmentRequestContextType, targetAlignment: TargetAlignment, alignmentResponse: AlignmentResponse) => Promise<Partial<RcsbFvRowConfigInterface>>;
+    readonly trackConfigModifier?: (alignmentContext: AlignmentRequestContextType, targetAlignment: TargetAlignment, alignmentResponse: AlignmentResponse, indexResponse: number) => Promise<Partial<RcsbFvRowConfigInterface>>;
 
     constructor(
         alignmentTrackFactory: TrackFactoryInterface<[AlignmentRequestContextType, TargetAlignment]>,
-        trackModifier?: (alignmentContext: AlignmentRequestContextType, targetAlignment: TargetAlignment, alignmentResponse: AlignmentResponse) => Promise<Partial<RcsbFvRowConfigInterface>>
+        trackModifier?: (alignmentContext: AlignmentRequestContextType, targetAlignment: TargetAlignment, alignmentResponse: AlignmentResponse, indexResponse: number) => Promise<Partial<RcsbFvRowConfigInterface>>
     ) {
         this.trackFactory = alignmentTrackFactory;
         this.trackConfigModifier = trackModifier;
@@ -24,14 +24,14 @@ export class AlignmentBlockFactory implements BlockFactoryInterface<[AlignmentRe
         alignmentRequestContext = {...alignmentRequestContext, querySequence: alignmentData.query_sequence ?? undefined};
         if(!alignmentData.target_alignment)
             return [];
-        return (await Promise.all<undefined|RcsbFvRowConfigInterface>(alignmentData.target_alignment.map(async alignment=>{
+        return (await Promise.all<undefined|RcsbFvRowConfigInterface>(alignmentData.target_alignment.map(async (alignment, index)=>{
             if (alignmentRequestContext.filterByTargetContains != null && !alignment?.target_id?.includes(alignmentRequestContext.filterByTargetContains))
                 return;
             if (alignment?.target_sequence == null)
                 return;
            return {
                ... await this.trackFactory.getTrack(alignmentRequestContext, alignment),
-               ... await this.trackConfigModifier?.(alignmentRequestContext,alignment, alignmentData)
+               ... await this.trackConfigModifier?.(alignmentRequestContext,alignment, alignmentData, index)
            };
         }))).filter((x): x is RcsbFvRowConfigInterface=>x!=undefined);
     }
