8a67f5fe0b4d5f0b7aadf354231599d30a291b58
bioinsilico <jsegurmor@gmail.com>
2023-09-07 13:55:17 -0700

    v5.1.0

--- src/RcsbChartWeb/HistogramChartComponent.tsx
+++ src/RcsbChartWeb/HistogramChartComponent.tsx
@@ -1,9 +1,6 @@
 import {AbstractChartComponent} from "./AbsractChartComponent";
 import React from "react";
 import {ChartComponent} from "@rcsb/rcsb-charts/lib/RcsbChartComponent/ChartComponent";
-import {
-    VictoryHistogramChartComponent
-} from "@rcsb/rcsb-charts/lib/RcsbChartImplementations/VictoryChartImplementations/VictoryHistogramChartComponent";
 import {HistogramChartDataProvider} from "@rcsb/rcsb-charts/lib/RcsbChartDataProvider/HistogramChartDataProvider";
 import {
     ChartJsHistogramComponent
@@ -14,7 +11,7 @@ export class HistogramChartComponent extends AbstractChartComponent {
     render(): JSX.Element {
         return (<ChartComponent
             data={this.state.data.map(d=>d.map(d=>({...d, label: (parseFloat(d.label.toString()) + (this.props.chartConfig?.histogramBinIncrement ?? 0)*0.5)})))}
-            chartComponentImplementation={VictoryHistogramChartComponent}
+            chartComponentImplementation={ChartJsHistogramComponent}
             dataProvider={new HistogramChartDataProvider()}
             chartConfig={this.props.chartConfig}
         />);
